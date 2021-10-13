66c20ce4607bd1a8089408b9a3b7c9acf1fdcba3
Nusiq <nusiq@outlook.com>
2021-10-13 20:33:49 +0200

    subfunctions:added support for some of the python build-in functions

--- subfunctions/safe_eval.py
+++ subfunctions/safe_eval.py
@@ -44,10 +44,34 @@ operators = {
     # Subscript (hardcoded)
 }
 
+scope_funcs = {
+    "abs": abs, # Returns the absolute value of a number
+    "all": all, # Returns True if all items in an iterable object are true
+    "any": any, # Returns True if any item in an iterable object is true
+    "bool": bool, # Returns the boolean value of the specified object
+    "complex": complex, # Returns a complex number
+    "float": float, # Returns a floating point number
+    "hex": hex, # Converts a number into a hexadecimal value
+    "int": int, # Returns an integer number
+    "len": len, # Returns the length of an object
+    "max": max, # Returns the largest item in an iterable
+    "min": min, # Returns the smallest item in an iterable
+    "oct": oct, # Converts a number into an octal
+    "pow": pow, # Returns the value of x to the power of y
+    "range": range, # Returns a sequence of numbers, starting from 0 and increments by 1 (by default)
+    "reversed": reversed, # Returns a reversed iterator
+    "round": round, # Rounds a numbers
+    "sorted": sorted, # Returns a sorted list
+    "str": str, # Returns a string object
+    "sum": sum, # Sums the items of an iterator
+    "zip": zip, # Returns an iterator, from two or more iterators
+}
+
 def safe_eval(expr, scope: Dict[str, int]):
     return _eval(ast.parse(expr, mode='eval').body, scope)
 
 def _eval(node, scope: Dict[str, int]):
+    scope = scope_funcs | scope
     if isinstance(node, ast.Constant):
         return node.value
     if isinstance(node, ast.Name):
@@ -81,6 +105,11 @@ def _eval(node, scope: Dict[str, int]):
             eval_val = _eval(val, scope)
             if not eval_val:  # TODO - other values should be checked to see if they're in scope (but not evaluated)
                 return eval_val
+    if isinstance(node, ast.Call):
+        func = _eval(node.func, scope)
+        args = [_eval(arg, scope) for arg in node.args]
+        kwargs = {kw.arg: _eval(kw.value, scope) for kw in node.keywords}
+        return func(*args, **kwargs)
     if isinstance(node, ast.Subscript):
         return _eval(node.value, scope)[_eval(node.slice, scope)]
     if isinstance(node, ast.UnaryOp): # <operator> <operand> e.g., -1
