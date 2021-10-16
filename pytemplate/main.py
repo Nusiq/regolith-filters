b0f95ce56ac5fe56899e16dfaf7f2bcf7d427559
Nusiq <nusiq@outlook.com>
2021-10-16 23:49:32 +0200

    pytemplate: cleaning up the code

--- pytemplate/main.py
+++ pytemplate/main.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Tuple
+from typing import Any, Dict, List, Tuple
 import merge
 import json
 import sys
@@ -8,12 +8,14 @@ import math
 import uuid
 
 
+DATA_PATH = Path('data')
+BP_PATH = Path('BP')
+RP_PATH = Path('RP')
+
+
 def walk_json(data, json_path=None):
     '''
-    Walks JSON file (data) yields tuples: key, parent_key, parent.
-
-    Hint: To access child value use parent[parent_key][child_key], in first
-    iteration 'parent' and 'parent_key' are None.
+    Walks JSON file (data) yields json paths.
     '''
     if json_path is None:
         json_path = []
@@ -22,20 +24,22 @@ def walk_json(data, json_path=None):
             curr_path = json_path + [k]
             if isinstance(data[k], (dict, list)):
                 yield from walk_json(data[k], json_path=curr_path)
-            yield curr_path
+            yield tuple(curr_path)
     elif isinstance(data, list):
         for i in range(len(data)):
             curr_path = json_path + [i]
             if isinstance(data[i], (dict, list)):
                 yield from walk_json(data[i], json_path=curr_path)
-            yield curr_path
+            yield tuple(curr_path)
 
 def access_json(data, path):
     if len(path) == 0:
         return data
     return access_json(data[path[0]], path[1:])
 
-def replace_templates(data, scope, trigger_phrases: List[str]) -> Tuple[Dict, bool]:
+def replace_templates(
+        data: Any, scope: Dict[str, Any], templates: Dict[str, str],
+        trigger_phrases: List[str]) -> Tuple[Dict, bool]:
     # Gather points of iterests (things that will be replaced)
     points_of_interset = []
     for poi in walk_json(data):
@@ -43,48 +47,49 @@ def replace_templates(data, scope, trigger_phrases: List[str]) -> Tuple[Dict, bo
         for trigger_phrase in trigger_phrases:
             if isinstance(k, str) and k.startswith(trigger_phrase):
                 points_of_interset.append(poi)
-    if len(points_of_interset) == 0:
+    if len(points_of_interset) == 0:  # No points of interests, return
         return data, False
-    for poi in points_of_interset:
+    for poi in sorted(points_of_interset):
+        # Access template
         try:
             k = poi[-1]
             template_name = k.split(":", 1)[1]
-            template_path = Path('data') / templates_path / (template_name + '.py')
-            with template_path.open('r') as f:
-                template = f.read()
-        except Exception as e:
-            raise RuntimeError(
-                f"Unable to read template {k}") from e
+            template = templates[template_name]
+        except KeyError as e:
+            raise RuntimeError(f"Unable to read template {k}") from e
+
+        # Apply template
         if len(poi) == 1:  # Templating root
             try:
                 curr_scope = scope | data[poi[0]]
             except KeyError:
-                # This key must have been replaced already by the recursive
-                # template support (it happens sometimes because the recursive)
-                # templates can add items at the same depth level as they are.
-                continue
+                continue  # Replaced by the recursive template execution, skip
+            # delete POI
             del data[poi[0]]
+            # Merge with template
             data = merge.deep_merge_objects(
-                eval(template, curr_scope), data,  merge.ListMergePolicy.APPEND)
-            # Support recursive templates
+                a=eval(template, curr_scope),
+                b=data, 
+                list_merge_policy=merge.ListMergePolicy.APPEND)
+            # Run recursive templating
             data, _ = replace_templates(
-                data, curr_scope, trigger_phrases)
-            continue
-        # Templating offspring
-        parent = access_json(data, poi[:-2])
-        try:
-            curr_scope = scope |  access_json(parent, poi[-2:])
-        except KeyError:
-            # This key must have been replaced already by the recursive
-            # template support (it happens sometimes because the recursive)
-            # templates can add items at the same depth level as they are.
-            continue
-        del parent[poi[-2]][poi[-1]]
-        parent[poi[-2]] = merge.deep_merge_objects(
-            eval(template, curr_scope), parent[poi[-2]],  merge.ListMergePolicy.APPEND)
-        # Support ecursive templates
-        parent[poi[-2]], _ = replace_templates(
-            parent[poi[-2]], curr_scope, trigger_phrases)
+                data, curr_scope, templates, trigger_phrases)
+        else:  # Templating offspring
+            parent = access_json(data, poi[:-2])
+            try:
+                curr_scope = scope |  access_json(parent, poi[-2:])
+            except KeyError:
+                continue  # Replaced by the recursive template execution, skip
+            # delete POI
+            del parent[poi[-2]][poi[-1]]
+            # Merge with template
+            parent[poi[-2]] = merge.deep_merge_objects(
+                a=eval(template, curr_scope),
+                b=parent[poi[-2]],
+                list_merge_policy=merge.ListMergePolicy.APPEND)
+            # Run recursive templating
+            parent[poi[-2]], _ = replace_templates(
+                parent[poi[-2]], curr_scope, templates, trigger_phrases)
     return data, True
 
 def main(
@@ -94,21 +99,27 @@ def main(
     Main function of the project. Adds filters to behavior- and resource-pack
     files. Read README for mor information.
     '''
-    # Resolve glob patterns
-    bp_paths = []
+    # Resolve glob patterns for BP  and RP
+    bp_paths = set()
     for i in bp_patterns:
-        bp_paths.extend(j for j in Path('BP').glob(i))
-    bp_paths = list(set(bp_paths))
-    rp_paths = []
+        bp_paths.update(BP_PATH.glob(i))
+    rp_paths = set()
     for i in rp_patterns:
-        rp_paths.extend(j for j in Path('RP').glob(i))
-    rp_paths = list(set(rp_paths))
+        rp_paths.update(RP_PATH.glob(i))
+
+    # Load the template files
+    templates: Dict[str, str] = {}
+    tp = DATA_PATH / templates_path
+    for template_path in tp.glob("**/*.py"):
+        key = template_path.relative_to(tp).with_suffix("").as_posix()
+        with template_path.open('r') as f:
+            templates[key] = f.read()
 
     fp: Path
     # Replace values in file using templates
     for fp in chain(bp_paths, rp_paths):
         if not fp.exists() or not fp.is_file():
-            continue 
+            continue
         try:
             with fp.open('r') as f:
                 data = json.load(f)
@@ -116,10 +127,8 @@ def main(
             print(f"Unable to load file {fp.as_posix()}")
             continue
 
-        try:
-            data, modified = replace_templates(data, scope, trigger_phrases)
-        except Exception as e:
-            raise RuntimeError(f"File: {fp.as_posix()}: {str(e)}") from e
+        data, modified = replace_templates(
+            data, scope, templates, trigger_phrases)
         if not modified:
             continue  # Data not modified. Don't edit the file.
 
@@ -164,7 +173,15 @@ if __name__ == '__main__':
     scope = {'true': True, 'false': False, 'math': math, 'uuid': uuid}
     if 'scope_path' not in config:
         config['scope_path'] = 'pytemplate/scope.json'
-    with (Path('data') / config['scope_path']).open('r') as f:
+    with (DATA_PATH / config['scope_path']).open('r') as f:
         scope = scope | json.load(f)
 
-    main(bp_patterns, rp_patterns, templates_path, trigger_phrases, sort_keys, compact, scope)
+    main(
+        bp_patterns=bp_patterns,
+        rp_patterns=rp_patterns,
+        templates_path=templates_path,
+        trigger_phrases=trigger_phrases,
+        sort_keys=sort_keys,
+        compact=compact,
+        scope=scope,
+    )
