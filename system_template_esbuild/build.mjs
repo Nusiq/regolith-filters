ee6a3ee1497d24b24751760b7f2d14ba1ab78049
Nusiq <nusiq@outlook.com>
2025-03-08 09:55:13 +0100

    system_template_esbuild: 3.0.0 - big changes: - Removed entryPoint property. - Added working_dir property. - The new configuration makes it easier to set up with the Minecraft debugger.

--- system_template_esbuild/build.mjs
+++ system_template_esbuild/build.mjs
@@ -1,13 +1,32 @@
 // @ts-check
-import { readFileSync } from "node:fs";
-import { build } from "esbuild";
+import {
+	readFileSync,
+	copyFileSync,
+	existsSync,
+	mkdirSync,
+	writeFileSync,
+	rmSync,
+	readdirSync,
+	statSync,
+} from "node:fs";
+import { join, dirname } from "node:path";
+import { buildSync } from "esbuild";
 import { parse } from "jsonc-parser";
+
+const REGOLITH_ROOT_DIR = process.env.ROOT_DIR;
+if (REGOLITH_ROOT_DIR === undefined) {
+	// This should never happen
+	console.error("The ROOT_DIR environment variable is not set");
+	process.exit(1);
+}
 
 const defaultSettings = {
 	minify: false,
-	entryPoint: "data/system_template_esbuild/main.ts",
+	/** @type {null | string} */
+	working_dir: null,
 	outfile: "BP/scripts/main.js",
 	external: ["@minecraft/server"],
+	/** @type {null | string} */
 	scope_path: null,
 	sourcemap: false,
 };
@@ -43,33 +62,46 @@ if (args !== undefined) {
 }
 
 // Extract the settings and check the types
-let { minify, entryPoint, outfile, external, scope_path, sourcemap } = settings;
+let {
+	minify,
+	outfile,
+	external,
+	scope_path,
+	sourcemap,
+	working_dir: working_dir,
+} = settings;
 
 if (typeof minify !== "boolean") {
-	throw new Error("The 'minify' setting must be a boolean");
-}
-if (typeof entryPoint !== "string") {
-	throw new Error("The 'entryPoint' setting must be a string");
+	console.error("The 'minify' setting must be a boolean");
+	process.exit(1);
 }
 if (typeof outfile !== "string") {
-	throw new Error("The 'outfile' setting must be a string");
+	console.error("The 'outfile' setting must be a string");
+	process.exit(1);
 }
 if (!Array.isArray(external)) {
-	throw new Error("The 'external' setting must be an array");
+	console.error("The 'external' setting must be an array");
+	process.exit(1);
 } else {
 	for (let ext of external) {
 		if (typeof ext !== "string") {
-			throw new Error("The 'external' setting must be an array of strings");
+			console.error("The 'external' setting must be an array of strings");
+			process.exit(1);
 		}
 	}
 }
 if (scope_path !== null && typeof scope_path !== "string") {
-	throw new Error("The 'scope_path' setting must be a string or null");
+	console.error("The 'scope_path' setting must be a string or null");
+	process.exit(1);
 }
 if (typeof sourcemap !== "boolean") {
-	throw new Error("The 'sourcemap' setting must be a boolean");
+	console.error("The 'sourcemap' setting must be a boolean");
+	process.exit(1);
 }
-
+if (working_dir !== null && typeof working_dir !== "string") {
+	console.error("The 'working_dir' setting must be a string or null");
+	process.exit(1);
+}
 // Append @minecraft/server to the external array if it's not already there
 if (!external.includes("@minecraft/server")) {
 	external.push("@minecraft/server");
@@ -78,25 +110,111 @@ if (!external.includes("@minecraft/server")) {
 // Load the scope from the scope file
 let scope = {};
 if (scope_path !== null) {
-	scope = parse(readFileSync("data/" + scope_path, "utf8"));
+	scope = parse(readFileSync(join("data", scope_path), "utf8"));
 }
 
 // Evaluate the settings with the scope
-entryPoint = evalString(entryPoint, scope);
 outfile = evalString(outfile, scope);
 
 // Build the project.
+if (working_dir === null) {
+	// Build into the data/system_template_esbuild/main.js file
+	const buildResult = buildSync({
+		external: external,
+		entryPoints: ["data/system_template_esbuild/main.ts"],
+		target: "es2020",
+		format: "esm",
+		bundle: true,
+		minify: minify,
+		outfile: "data/system_template_esbuild/main.js",
+		sourcemap: sourcemap,
+	});
+	if (buildResult.errors.length > 0) {
+		console.error(buildResult.errors);
+		process.exit(1);
+	}
 
-build({
-	external: external,
-	entryPoints: [entryPoint],
-	target: "es2020",
-	format: "esm",
-	bundle: true,
-	minify: minify,
-	outfile: outfile,
-	sourcemap: sourcemap,
-}).catch((err) => {
-	console.error(err.message);
-	process.exit(1);
-});
+	// Copy the generated main.js file to the 'outfile' location
+	mkdirSync(dirname(outfile), { recursive: true });
+	copyFileSync("data/system_template_esbuild/main.js", outfile);
+} else {
+	const dotSystemTemplateEsbuildText =
+		"This file marks the directory as safe for deletion by " +
+		"system_template_esbuild filter.";
+
+	// Resolve the working directory
+	working_dir = join(REGOLITH_ROOT_DIR, working_dir);
+	// Check if the working directory exists, if it does, try to clean it safely
+	// by checking if it's empty or marked with a .system_template_esbuild file
+	if (existsSync(working_dir)) {
+		// Make sure it's a directory
+		if (!statSync(working_dir).isDirectory()) {
+			console.error(
+				"The working directory path is not a directory.\n" +
+					`Path: ${working_dir}`
+			);
+			process.exit(1);
+		}
+
+		// Check if empty or marked with .system_template_esbuild file
+		if (readdirSync(working_dir).length !== 0) {
+			if (existsSync(join(working_dir, ".system_template_esbuild"))) {
+				rmSync(working_dir, { recursive: true, force: true });
+				mkdirSync(working_dir);
+			} else {
+				console.error(
+					"The working directory couldn't be safely cleaned. It's not empty " +
+						"and doesn't seem to be created by system_template_esbuild.\n" +
+						`Path: ${working_dir}`
+				);
+				process.exit(1);
+			}
+		}
+	} else {
+		mkdirSync(working_dir);
+	}
+
+	// Copy the files form data/system_template_esbuild to the working directory
+	const files = /** @type {string[]} */ (
+		readdirSync("data/system_template_esbuild", {
+			recursive: true,
+		})
+	);
+	for (const file of files) {
+		const srcPath = join("data/system_template_esbuild", file);
+		const destPath = join(working_dir, file);
+
+		// Create parent directories if they don't exist
+		mkdirSync(dirname(destPath), { recursive: true });
+
+		// Copy
+		if (!statSync(srcPath).isDirectory()) {
+			copyFileSync(srcPath, destPath);
+		}
+	}
+
+	// Create marker file
+	writeFileSync(
+		join(working_dir, ".system_template_esbuild"),
+		dotSystemTemplateEsbuildText
+	);
+
+	// Build in the working directory
+	const buildResult = buildSync({
+		external: external,
+		entryPoints: [join(working_dir, "main.ts")],
+		target: "es2020",
+		format: "esm",
+		bundle: true,
+		minify: minify,
+		outfile: join(working_dir, "main.js"),
+		sourcemap: sourcemap,
+	});
+	if (buildResult.errors.length > 0) {
+		console.error(buildResult.errors);
+		process.exit(1);
+	}
+	// Copy the generated main.js file to the 'outfile' location
+	mkdirSync(dirname(outfile), { recursive: true });
+	copyFileSync(join(working_dir, "main.js"), outfile);
+}
