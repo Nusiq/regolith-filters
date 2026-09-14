5ebde8d1897dd2bdb88c34be93e9c5e4a335d22d
Nusiq <nusiq@outlook.com>
2026-09-14 15:52:00 +0200

    modular_mc: Updated the default auto-map.ts, now the output paths are namespaced.

--- modular_mc/data/auto-map.ts
+++ modular_mc/data/auto-map.ts
@@ -1,3 +1,4 @@
+export const namespace = "my_namespace";
 export const AUTO_MAP = {
 	".bp_ac.json": "BP/animation_controllers",
 	".bp_anim.json": "BP/animations",
@@ -13,14 +14,29 @@ export const AUTO_MAP = {
 		path: "BP/features",
 		extension: ".json",
 	},
-	".mcfunction": "BP/functions",
+	".mcfunction": `BP/functions/${namespace}`,
 	".bp_item.json": "BP/items",
 	".item.json": "BP/items",
-	".loot.json": "BP/loot_tables",
+	".loot.json": {
+		path: `BP/loot_tables/${namespace}`,
+		extension: ".json",
+	},
 	".recipe.json": "BP/recipes",
 	".spawn_rule.json": "BP/spawn_rules",
-	".mcstructure": "BP/structures",
-	".trade.json": "BP/trading",
+	".mcstructure": `BP/structures/${namespace}`,
+	".jigsaw.json": {
+		path: "BP/worldgen/structures",
+		extension: ".json",
+	},
+	".template_pool.json": {
+		path: "BP/worldgen/template_pools",
+		extension: ".json",
+	},
+	".structure_set.json": {
+		path: "BP/worldgen/structure_sets",
+		extension: ".json",
+	},
+	".trade.json": `BP/trading/${namespace}`,
 	".rp_ac.json": "RP/animation_controllers",
 	".animation.json": "RP/animations",
 	".attachable.json": "RP/attachables",
@@ -31,53 +47,73 @@ export const AUTO_MAP = {
 	".geo.json": "RP/models/entity",
 	".particle.json": "RP/particles",
 	".rc.json": "RP/render_controllers",
-	".fsb": "RP/sounds",
-	".mp4": "RP/sounds",
-	".ogg": "RP/sounds",
-	".wav": "RP/sounds",
+	".fsb": `RP/sounds/${namespace}`,
+	".mp4": `RP/sounds/${namespace}`,
+	".ogg": `RP/sounds/${namespace}`,
+	".wav": `RP/sounds/${namespace}`,
 	".lang": "RP/texts",
 	".attachable.tga": {
-		path: "RP/textures/attachables",
+		path: `RP/textures/${namespace}/attachables`,
 		extension: ".tga",
 	},
 	".block.tga": {
-		path: "RP/textures/blocks",
+		path: `RP/textures/${namespace}/blocks`,
 		extension: ".tga",
 	},
 	".item.tga": {
-		path: "RP/textures/items",
+		path: `RP/textures/${namespace}/items`,
 		extension: ".tga",
 	},
 	".entity.tga": {
-		path: "RP/textures/entity",
+		path: `RP/textures/${namespace}/entity`,
 		extension: ".tga",
 	},
 	".particle.tga": {
-		path: "RP/textures/particle",
+		path: `RP/textures/${namespace}/particle`,
 		extension: ".tga",
 	},
 	".attachable.png": {
-		path: "RP/textures/attachables",
+		path: `RP/textures/${namespace}/attachables`,
 		extension: ".png",
+	},
+	".attachable.texture_set.json": {
+		path: `RP/textures/${namespace}/attachables`,
+		extension: ".texture_set.json",
 	},
 	".block.png": {
-		path: "RP/textures/blocks",
+		path: `RP/textures/${namespace}/blocks`,
 		extension: ".png",
+	},
+	".block.texture_set.json": {
+		path: `RP/textures/${namespace}/blocks`,
+		extension: ".texture_set.json",
 	},
 	".item.png": {
-		path: "RP/textures/items",
+		path: `RP/textures/${namespace}/items`,
 		extension: ".png",
+	},
+	".item.texture_set.json": {
+		path: `RP/textures/${namespace}/items`,
+		extension: ".texture_set.json",
 	},
 	".entity.png": {
-		path: "RP/textures/entity",
+		path: `RP/textures/${namespace}/entity`,
 		extension: ".png",
+	},
+	".entity.texture_set.json": {
+		path: `RP/textures/${namespace}/entity`,
+		extension: ".texture_set.json",
 	},
 	".particle.png": {
-		path: "RP/textures/particle",
+		path: `RP/textures/${namespace}/particle`,
 		extension: ".png",
 	},
+	".particle.texture_set.json": {
+		path: `RP/textures/${namespace}/particle`,
+		extension: ".texture_set.json",
+	},
 	".ui.png": {
-		path: "RP/textures/ui",
+		path: `RP/textures/${namespace}/ui`,
 		extension: ".png",
 	},
 	".js": "data/system_template_esbuild",
